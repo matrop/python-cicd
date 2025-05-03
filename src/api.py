73d8c29e9753201501fdf56521b7aa5069974cbf
Maurice Atrops <matrops@cologne-intelligence.com>
2025-05-03 16:08:53 +0200

    fix: apply formatting

--- src/api.py
+++ src/api.py
@@ -5,9 +5,10 @@ from fastapi import FastAPI, HTTPException
 
 from .classes import Task
 
-app = FastAPI() 
+app = FastAPI()
 
 task_list: List[Task] = []
+
 
 def get_new_id(task_list: List[Task]):
     if not task_list:
@@ -15,56 +16,45 @@ def get_new_id(task_list: List[Task]):
 
     return task_list[-1].id + 1
 
+
 def get_task_by_id(task_list: List[Task], id: int) -> Optional[Task]:
     for task in task_list:
         if id == task.id:
             return task
 
-    raise ValueError(f"Could not find task with id '{id}'") 
+    raise ValueError(f"Could not find task with id '{id}'")
+
 
 def get_tasks_by_tag(task_list: List[Task], tag: str) -> List[Task]:
     return [task for task in task_list if tag in task.tags]
 
+
 @app.post("/tasks")
-def create_task(name: str, description: Optional[str]=None, tags: Optional[List[str]]=None):
+def create_task(
+    name: str, description: Optional[str] = None, tags: Optional[List[str]] = None
+):
     id = get_new_id(task_list)
 
-    new_task = Task(
-        id=id, 
-        name=name,
-        description=description,
-        tags=tags
-    )
+    new_task = Task(id=id, name=name, description=description, tags=tags)
     task_list.append(new_task)
 
     return new_task.model_dump()
+
 
 @app.delete("/tasks/{id}")
 def delete_task(id: int):
     try:
         task = get_task_by_id(task_list, id)
     except ValueError as e:
-        raise HTTPException(
-            status_code=404, 
-            detail=str(e)
-        )
-        
+        raise HTTPException(status_code=404, detail=str(e))
+
     task_list.remove(task)
 
     return {"result": f"task id '{id}' deleted"}
 
+
 @app.get("/tasks")
 def list_tasks(tag: Optional[str] = None):
     if tag:
-        return {
-            "tasks": [
-                task.dict() for task in get_tasks_by_tag(task_list, tag)
-            ]    
-        }
-    return {
-        "tasks": [
-            task.dict() for task in task_list
-        ]
-    }
-
-
+        return {"tasks": [task.dict() for task in get_tasks_by_tag(task_list, tag)]}
+    return {"tasks": [task.dict() for task in task_list]}
--- src/classes.py
+++ src/classes.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional, List
 
+
 class Task(BaseModel):
     id: int
     name: str
