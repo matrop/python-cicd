35cf3f258f9c264fee1d3807155402fb161051e3
Maurice Atrops <matrops@cologne-intelligence.com>
2025-05-11 12:15:16 +0200

    wip: add progress [skip ci]

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    use_oidc             = true
+    use_azuread_auth     = true
+    storage_account_name = "samauriceatropsdev" # TODO: How to account for different environments?
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
